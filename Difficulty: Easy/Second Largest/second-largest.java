5be1a6acf038ac9037edfbbe6ed1a80513a4eae9
Priyanshu Sharma <143121050+PriyanshuSharma007@users.noreply.github.com>
2025-07-16 02:40:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,44 +1,23 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
-// User function Template for Java
-
 class Solution {
     public int getSecondLargest(int[] arr) {
-        // Code Here
-        int max=Integer.MIN_VALUE;
+        // code here
+        int largest=Integer.MIN_VALUE;
+        int seclargest=Integer.MIN_VALUE; 
         for(int i=0;i<arr.length;i++){
-            max=Math.max(max,arr[i]);
-        }
-        int secMax=-1;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>secMax && arr[i]<max){
-            secMax=arr[i];
+            if(largest<arr[i]){
+                seclargest=largest;
+                largest=arr[i];
+            }
+            else if(seclargest<arr[i] && arr[i]!=largest){
+                seclargest=arr[i];
             }
         }
-        return secMax;
-    } 
+        if(seclargest == Integer.MIN_VALUE){
+            return -1;
+        }
+        else {
+            return seclargest;
+        }
+        
+    }
 }
